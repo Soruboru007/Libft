1476df5bc192f9b9cea8af0b070d516f912c9923
Soru Boru <113847544+Soruboru007@users.noreply.github.com>
2024-05-13 13:33:37 +0900

    Update ft_isascii.c

--- libft_part1/ft_isascii.c
+++ libft_part1/ft_isascii.c
@@ -15,13 +15,8 @@
 
 int	ft_isascii(int c)
 {
-	if (0 <= c && c <= 127)
-	{
-		return (1);
-	}
-	return (0);
+	return (c >= 0 && c <= 127);
 }
-
 int main (void)
 {
 	printf("%d\n",ft_isascii('a'));
